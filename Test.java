bb49f7807335a85e419e256ac8dcc1f29aa55748
agungrio307 <agungrio307@gmail.com>
2025-10-01 14:30:58 +0700

    anyarrr

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Agung Imut Banget");
-        System.out.println("Ditooo Gantenggg");
+        System.out.println("Ditooo Ayuuuu");
+        System.out.println("Dito gantenggg");
     }
 }
